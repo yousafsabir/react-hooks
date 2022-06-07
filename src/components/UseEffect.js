544fe93eb635de537dd17065a14d1884d2124e52
yousafsabir <yousafsabir430516@gmail.com>
2022-06-07 15:20:20 +0500

    useEffect done

--- src/components/UseEffect.js
+++ src/components/UseEffect.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 
 const UseEffect = () => {
-    const [resource, setResource] = useState("posts");
+    // Styling
     const Styles = {
         flex: {
             display: "flex",
@@ -18,15 +18,37 @@ const UseEffect = () => {
             backgroundColor: "#3a3a3a",
         },
     };
+    const [resource, setResource] = useState("Posts");
+    const [items, setItems] = useState([]);
+
+    useEffect(() => {
+        fetch(`https://jsonplaceholder.typicode.com/${resource}`)
+            .then((response) => response.json())
+            .then((json) => setItems(json));
+    }, [resource]);
+
+    // return the code to terminate effect on component unmounts
+    useEffect(() => {
+        const unsub = () => {
+            console.log("when component unmounts");
+        };
+        return unsub;
+    }, [resource]);
+
     return (
         <div style={Styles.container}>
             <hr />
             <div style={Styles.flex}>
-                <button>Posts</button>
-                <button>Comments</button>
-                <button>Users</button>
-                <div>{resource}</div>
+                <button onClick={() => setResource("Posts")}>Posts</button>
+                <button onClick={() => setResource("Comments")}>
+                    Comments
+                </button>
+                <button onClick={() => setResource("Users")}>Users</button>
+                <h2>{resource}</h2>
             </div>
+            {items.map((item) => {
+                return <pre key={item.id}>{JSON.stringify(item)}</pre>;
+            })}
             <hr />
         </div>
     );
